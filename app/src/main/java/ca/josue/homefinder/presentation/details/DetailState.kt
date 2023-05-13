77b9a9bbdad80f8bb26fac2953cafa596d797535
Josue Lubaki <josuelubaki@gmail.com>
2023-05-13 03:09:16 -0400

    compressed video & fixed ksp version

--- app/src/main/java/ca/josue/homefinder/presentation/details/DetailState.kt
+++ app/src/main/java/ca/josue/homefinder/presentation/details/DetailState.kt
@@ -4,8 +4,8 @@ import androidx.annotation.StringRes
 import ca.josue.homefinder.domain.models.House
 
 sealed class DetailState {
-    data object Idle : DetailState()
-    data object Loading : DetailState()
+    object Idle : DetailState()
+    object Loading : DetailState()
     data class Success(val house: House) : DetailState()
     data class Error(@StringRes val message: Int) : DetailState()
 }
